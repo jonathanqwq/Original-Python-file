dac09a470c1f5e223d539b9becd1adafb56e6ae6
jonathanqwq <63578859+jonathanqwq@users.noreply.github.com>
2020-08-04 10:20:40 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+import os
+age=input("请输入你的年龄：")
+name=input("请输入你的名字：")
+print("你的年龄是：")
+print(age)
+print("你的名字是：")
+print(name)
+os.system("pause")
+print("请一定输入数字！否则系统将出现问题！")
+num = int(input("请输入：1+1="))
+if(num>0):
+	if(num>2):
+		print("大了哦")
+	elif(num<2):
+		print("小了哦")
+	else:
+		print("正确")
+else:
+	print("请输入正确数字！")
+os.system("pause")
